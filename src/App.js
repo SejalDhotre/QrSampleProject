bcb8cb288d3e244696cbd5171df20624c34c2138
SejalDhotre <dhotresejal@gmail.com>
2023-08-31 11:04:28 +0530

    Unknown changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React  from "react";
+import QRCode from "react-qr-code";
+import {useState } from "react";
 import './App.css';
 
-function App() {
+function QrCodeInfo() {
+  const [wifiName,setWifiName]=useState("");
+  const [password,setPassword]=useState("");
+  const [qrCodeVisible,setQrCodeVisible]=useState(false);
+
+  const clickButton=() => {
+    let qrimage = password;
+    setQrCodeVisible(!!qrimage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <div className="center card shadow">
+        <div className="flex">
+          <form>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">WifiName:</span>
+                <input type="text" placeholder="Enter your WifiName" value={wifiName} onChange={(event)=>setWifiName(event.target.value)}
+                required />
+              </div>
+              </div>
+              <div className="user-details">
+              <div className="input-box">
+                <span className="details">Password:</span>
+                <input type="password"  value={password} onChange={(event)=>setPassword(event.target.value)}
+                required />
+              </div>
+            </div>
+            <div className="button">
+              <button type="button" className="center" onClick={clickButton}>
+              Submit
+              </button>
+            </div>
+          </form>
+          <div className="sample">
+            <h1>{wifiName}</h1>
+            {qrCodeVisible && <QRCode value={password}/>}
+          </div>
+        </div>
+      </div>  
     </div>
   );
 }
 
-export default App;
+export default QrCodeInfo;
